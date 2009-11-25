04ca7ecc1d8db200146f5a33a48ed7b3c2120a63
Raphael Bartolome <raphael.bartolome@gmail.com>
2009-11-25 08:49:20 +0100

    adding header file templates

---
+++ File Templates/Cappuccino/Objective-J Header.pbfiletemplate/header.h
@@ -0,0 +1,13 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «ORGANIZATIONNAME» «YEAR». All rights reserved.
+//
+
+@interface «FILEBASENAMEASIDENTIFIER» : CPObject
+{
+}
+
+@end
